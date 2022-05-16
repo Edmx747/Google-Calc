ed170faca4f516b8d5b60ed97e329fdaf9906356
Demircan Erol <edemircan@adista.fr>
2022-05-16 23:31:14 +0200

    wip: calculate front side

--- FrontEnd/src/screens/CalculatorScreen.tsx
+++ FrontEnd/src/screens/CalculatorScreen.tsx
@@ -1,25 +1,44 @@
-import React, { ReactElement, ReactNode } from 'react'
+import React, { ReactElement, ReactNode, useEffect, useState } from 'react'
 import CustomButton from '../components/CustomButton/CustomButton'
 import Display from '../components/Display.tsx/Display'
 import { CALCULATOR_BUTTONS } from '../helpers/CalculatorButtons';
-import { ICalculatorButton } from '../models/CalculatorButtons';
+import { CalculatorButtonType, ICalculatorButton } from '../models/CalculatorButtons';
 import './CalculatorScreen.css'
 function CalculatorScreen() {
-    let [operation, setOperation] = React.useState('');
-    let [result, setResult] = React.useState('');
-    let [row, SetRows] = React.useState(1);
-    const handleOperation = (value: string): any => {
+    let [operation, setOperation] = useState('');
+    let [result, setResult] = useState('');
+    let [typed, setTyped] = useState<Array<ICalculatorButton>>([]);
+
+    const handleOperation = (button: ICalculatorButton): any => {
+        const { value, type } = button;
+        if (typed.length > 0 && typed[typed.length - 1].type === CalculatorButtonType.OPERATOR && type === CalculatorButtonType.OPERATOR) {
+            setTyped([...typed.splice(typed.length - 1, 1)]);
+        }
+        setTyped([...typed.concat(button)]);
+        setOperation(typed.map(button => button.value).join(''));
         if (value === '=') {
-            setResult(eval(operation));
-            console.log('operation:', operation)
-            setOperation('');
+            if (result !== '') {
+                setResult(eval(result + operation));
+                setOperation('');
+                setTyped([]);
+            } else {
+                setResult(eval(operation));
+                setOperation('')
+                setTyped([]);
+            }
         } else if (value === 'AC') {
             setOperation('');
             setResult('');
+            setTyped([]);
         } else {
-            setOperation(operation + value);
+            setOperation(eval(typed.map(button => button.value).join('')));
         }
     }
+
+    useEffect(() => {
+        setOperation(typed.map(button => button.value).join(''));
+    }, [typed])
+    
 
     return (
         <div className='calculator'>
@@ -33,20 +52,19 @@ function CalculatorScreen() {
                         </tr>
                     </thead>
                     {
-                    CALCULATOR_BUTTONS.map((buttons: ICalculatorButton[], index: number) => {
-                        return (
-                            <tr key={index}>
-                                {buttons.map((button: ICalculatorButton, index: number) => {
-                                    return (
-                                        <td key={index}>
-                                            <CustomButton value={button.value} className={button.className} onClick={() => handleOperation(button.value)}></CustomButton>
-                                        </td>
-                                    )
-                                })}
-                            </tr>
-
-                        )
-                    })}
+                        CALCULATOR_BUTTONS.map((buttons: ICalculatorButton[], index: number) => {
+                            return (
+                                <tr key={index}>
+                                    {buttons.map((button: ICalculatorButton, index: number) => {
+                                        return (
+                                            <td key={index}>
+                                                <CustomButton value={button.name ? button.name : button.value} className={button.className} onClick={() => handleOperation(button)}></CustomButton>
+                                            </td>
+                                        )
+                                    })}
+                                </tr>
+                            )
+                        })}
                 </tbody>
             </table>
         </div >
